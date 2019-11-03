b14891374b9b11b2dd4a29234bf8b8509c4da253
Wendel Santos <henrique.santos1988@gmail.com>
2019-11-03 12:07:12 -0300

    hello world

---
+++ src/main/java/com/wendel/brewer/config/WebConfig.java
@@ -0,0 +1,65 @@
+package com.wendel.brewer.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import com.wendel.brewer.controller.CervejasController;
+
+@Configuration
+@ComponentScan(basePackageClasses = { CervejasController.class })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+	
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+		
+	};
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+	
+	
+	@Bean
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		
+		return engine;
+	}
+	
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolve = new SpringResourceTemplateResolver();
+		resolve.setApplicationContext(applicationContext);
+		resolve.setPrefix("classpath:/templates/");
+		resolve.setSuffix(".html");
+		resolve.setTemplateMode(TemplateMode.HTML);
+		
+		return resolve;
+	}
+
+	
+
+	
+}
